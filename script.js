9cb122e936021c8791c170b55c3e4d20ee13a635
gudzdanil <gudz.danil@gmail.com>
2016-07-20 17:57:37 +0300

    asdf

--- script.js
+++ script.js
@@ -1,19 +1,35 @@
-var globalSW = null;
+// Register the ServiceWorker
 navigator.serviceWorker.register('cache-worker.js', {
     scope: '.'
-}).then(function (registration) {
-    navigator.serviceWorker.ready.then(function () {
-        globalSW = registration;
-        $(document.body).removeClass('loading');
+}).then(function(registration) {
+    // The service worker has been registered!
+});
+
+// Listen for claiming of our ServiceWorker
+navigator.serviceWorker.addEventListener('controllerchange', function(event) {
+    // Listen for changes in the state of our ServiceWorker
+    navigator.serviceWorker.controller.addEventListener('statechange', function() {
+        // If the ServiceWorker becomes "activated", let the user know they can go offline!
+        if (this.state === 'activated') {
+            // Show the "You may now use offline" notification
+            document.getElementById('offlineNotification').classList.remove('hidden');
+        }
     });
 });
 
+document.querySelector('#randomButton').addEventListener('click', function() {
+    var image = document.querySelector('#logoImage');
+    var currentIndex = Number(image.src.match('c-asteroids-([1-6])')[1]);
+    var newIndex = getRandomNumber();
 
-function startPushing() {
-    if (!globalSW) {
-        return;
+    // Ensure that we receive a different image than the current
+    while (newIndex === currentIndex) {
+        newIndex = getRandomNumber();
     }
-    for (var i = 0; i < globalSW.tokens.length; i++) {
-        globalSW.triggerPush(null, i);
+
+    image.src = 'img/c-asteroids-' + newIndex + '.png';
+
+    function getRandomNumber() {
+        return Math.floor(Math.random() * 6) + 1;
     }
-}
+});
