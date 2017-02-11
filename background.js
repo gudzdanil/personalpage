a5aaec4cf8eb7914d1775a87c2f34d09acc2abc1
danilgudz <gudz.danil@gmail.com>
2017-02-11 16:20:18 +0200

    onesignal

--- background.js
+++ background.js
@@ -1,4 +1,17 @@
 OneSignal.init({
-    appId: "ce842eda-dcf8-4c48-930e-208e6c7b0db4"
+    appId: ""
     // googleProjectNumber: "931838981561"
+});
+window.OneSignal = window.OneSignal || [];
+var initOptions = {
+    appId: "ce842eda-dcf8-4c48-930e-208e6c7b0db4",
+    subdomainName: 'gudz',
+    autoRegister: true,
+    notifyButton: {
+        enable: true
+    }
+};
+
+OneSignal.push(function() {
+    OneSignal.init(initOptions);
 });
