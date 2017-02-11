7c812d21080f1c94dd32def39257336b28fbbc94
danilgudz <gudz.danil@gmail.com>
2017-02-11 16:27:54 +0200

    onesignal

--- background.js
+++ background.js
@@ -1,7 +1,3 @@
-OneSignal.init({
-    appId: ""
-    // googleProjectNumber: "931838981561"
-});
 window.OneSignal = window.OneSignal || [];
 var initOptions = {
     appId: "ce842eda-dcf8-4c48-930e-208e6c7b0db4",
